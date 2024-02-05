a1a2c8b55fd8c3705c5df255d7221cc5311216bd
smazepaa <sof.mazepa@gmail.com>
2024-02-05 14:48:19 +0200

    server class (based on one in threads branch)

--- server/server/server.cpp
+++ server/server/server.cpp
@@ -11,78 +11,186 @@ using namespace std;
 mutex consoleMutex;
 vector<SOCKET> clients;
 
-void broadcastMessage(const string& message, SOCKET senderSocket) {
-	lock_guard<mutex> lock(consoleMutex);
-	string fullMessage = "Client " + to_string(senderSocket) + ": " + message;
-	cout << fullMessage << endl;
-	for (SOCKET client : clients) {
-		if (client != senderSocket) {
-			send(client, fullMessage.c_str(), fullMessage.size() + 1, 0);
-		}
-	}
-}
+class ConnectionManager {
 
-void handleClient(SOCKET clientSocket) {
-	clients.push_back(clientSocket);
-	char buffer[4096];
-	while (true) {
-		int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
-		if (bytesReceived <= 0) {
-			lock_guard<mutex> lock(consoleMutex);
-			cout << "Client " << clientSocket << " disconnected.\n";
-			break;
-		}
-		buffer[bytesReceived] = '\0';
-		string message(buffer);
-		broadcastMessage(message, clientSocket);
-	}
-	closesocket(clientSocket);
-}
+    SOCKET serverSocket;
+    sockaddr_in serverAddr;
+    int port = 12345;
+
+    bool winsockInit() {
+        WSADATA wsaData;
+        if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+            cerr << "WSAStartup failed" << endl;
+            return false;
+        }
+        return true;
+    }
+
+    void serverConfig() {
+        serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+        if (serverSocket == INVALID_SOCKET)
+        {
+            cerr << "Error creating socket: " << WSAGetLastError() << endl;
+            WSACleanup();
+            return;
+        }
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_addr.s_addr = INADDR_ANY;
+        serverAddr.sin_port = htons(port);
+
+        bindSocket();
+        listenForConnections();
+    }
+
+    void bindSocket() {
+        if (bind(serverSocket, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) == SOCKET_ERROR) {
+            cerr << "Bind failed with error: " << WSAGetLastError() << endl;
+            cleanup();
+            return;
+        }
+    }
+
+    void listenForConnections() {
+        if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR)
+        {
+            cerr << "Listen failed with error: " << WSAGetLastError() << endl;
+            closesocket(serverSocket);
+            WSACleanup();
+            return;
+        }
+        cout << "Server listening on port " << port << endl << endl;
+    }
+
+    void cleanup() {
+        closesocket(serverSocket);
+        WSACleanup();
+    }
+
+public:
+    ConnectionManager() {
+        if (!winsockInit()) {
+            throw runtime_error("Winsock initialization failed");
+        }
+        else {
+            serverConfig();
+        }
+    }
+
+    ~ConnectionManager() {
+        cleanup();
+    }
+
+    bool isReady() const {
+        return serverSocket != INVALID_SOCKET;
+    }
+
+    SOCKET acceptClient() {
+        SOCKET clientSocket = accept(serverSocket, nullptr, nullptr);
+        if (clientSocket == INVALID_SOCKET) {
+            cerr << "Accept failed with error: " << WSAGetLastError() << endl;
+            return INVALID_SOCKET;
+        }
+        return clientSocket;
+    }
+
+};
+
+class CommandHandler {
+
+    SOCKET clientSocket;
+    string serverDirectory;
+    string baseDirectory = "C:/Users/sofma/server-dir/";
+    string clientName;
+
+public:
+
+    CommandHandler() : clientSocket(INVALID_SOCKET) {}
+
+    CommandHandler(SOCKET& client) : clientSocket(client) {}
+
+    bool isReady() {
+        return this->clientSocket != INVALID_SOCKET;
+    }
+
+    void broadcastMessage(const string& message, SOCKET senderSocket) {
+        lock_guard<mutex> lock(consoleMutex);
+        string fullMessage = "Client " + to_string(senderSocket) + ": " + message;
+        cout << fullMessage << endl;
+        for (SOCKET client : clients) {
+            if (client != senderSocket) {
+                send(client, fullMessage.c_str(), fullMessage.size() + 1, 0);
+            }
+        }
+    }
+
+    void handleClient() {
+        clients.push_back(clientSocket);
+        char buffer[4096];
+        while (true) {
+            int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+            if (bytesReceived <= 0) {
+                lock_guard<mutex> lock(consoleMutex);
+                cout << "Client " << clientSocket << " disconnected.\n";
+                break;
+            }
+            buffer[bytesReceived] = '\0';
+            string message(buffer);
+            broadcastMessage(message, clientSocket);
+        }
+        closesocket(clientSocket);
+    }
+};
+
+
+class Server {
+    ConnectionManager connManager;
+    vector<thread> clientThreads;
+    CommandHandler cmdHandler;
+
+    void handleClient(SOCKET clientSocket) {
+
+        CommandHandler cmdHandler(clientSocket);
+        while (cmdHandler.isReady()) {
+            cmdHandler.handleClient();
+        }
+    }
+
+public:
+
+    ~Server() {
+        for (auto& t : clientThreads) {
+            if (t.joinable()) {
+                t.join();
+            }
+        }
+    }
+
+    void start() {
+        while (true) {
+            cout << "Waiting for a new client..." << endl;
+            SOCKET clientSocket = connManager.acceptClient();
+            if (clientSocket != INVALID_SOCKET) {
+                lock_guard<mutex> lock(consoleMutex);
+                cout << "Client " << clientSocket << " connected.\n";
+                thread clientThread([this, clientSocket]() {
+                    this->handleClient(clientSocket);});
+                clientThread.detach();
+            }
+            else {
+                cerr << "Failed to accept a client." << endl;
+            }
+        }
+    }
+};
 
 int main() {
-	WSADATA wsaData;
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		cerr << "WSAStartup failed.\n";
-		return 1;
-	}
-	SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-	if (serverSocket == INVALID_SOCKET) {
-		cerr << "Socket creation failed.\n";
-		WSACleanup();
-		return 1;
-	}
-	sockaddr_in serverAddr;
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_addr.s_addr = INADDR_ANY;
-	serverAddr.sin_port = htons(12345);
-	if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) ==
-		SOCKET_ERROR) {
-		cerr << "Bind failed.\n";
-		closesocket(serverSocket);
-		WSACleanup();
-		return 1;
-	}
-	if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
-		cerr << "Listen failed.\n";
-		closesocket(serverSocket);
-		WSACleanup();
-		return 1;
-	}
-	cout << "Server is listening on port 12345...\n";
-	while (true) {
-		SOCKET clientSocket = accept(serverSocket, nullptr, nullptr);
-		if (clientSocket == INVALID_SOCKET) {
-			cerr << "Accept failed.\n";
-			closesocket(serverSocket);
-			WSACleanup();
-			return 1;
-		}
-		lock_guard<mutex> lock(consoleMutex);
-		cout << "Client " << clientSocket << " connected.\n";
-		thread clientThread(handleClient, clientSocket);
-		clientThread.detach(); // Detach the thread to allow handling multiple clients concurrently
-	}
-	closesocket(serverSocket);
-	WSACleanup();
-	return 0;
+    try {
+        Server server;
+        server.start();
+    }
+    catch (const runtime_error& e) {
+        cerr << "Server Error: " << e.what() << endl;
+        return 1;
+    }
+    return 0;
 }
