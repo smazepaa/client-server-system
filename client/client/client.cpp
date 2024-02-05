f0f6179cd2bab01925895b4bd6e072db03d9cb34
smazepaa <sof.mazepa@gmail.com>
2024-02-05 14:12:16 +0200

    client class (modified from threads branch)

--- client/client/client.cpp
+++ client/client/client.cpp
@@ -5,54 +5,147 @@
 #include <Ws2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 
-void receiveMessages(SOCKET clientSocket) {
-	char buffer[4096];
-	while (true) {
-		int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
-		if (bytesReceived <= 0) {
-			std::cerr << "Server disconnected.\n";
-			break;
-		}
-		buffer[bytesReceived] = '\0';
-		// std::cout << "Server: " << buffer << std::endl;
-		std::cout << buffer << std::endl;
-	}
-}
+using namespace std;
+
+class ConnectionManager {
+
+    SOCKET clientSocket;
+    sockaddr_in serverAddr;
+    int port = 12345;
+    PCWSTR serverIp = L"127.0.0.1";
+
+    void clientConfig() {
+        clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+        if (clientSocket == INVALID_SOCKET) {
+            cerr << "Error creating socket: " << WSAGetLastError() << endl;
+            WSACleanup();
+            return;
+        }
+
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_port = htons(port);
+        InetPton(AF_INET, serverIp, &serverAddr.sin_addr);
+    }
+
+    bool winsockInit() {
+        WSADATA wsaData;
+        if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+            cerr << "WSAStartup failed" << endl;
+            return false;
+        }
+        return true;
+    }
+
+    void cleanup() {
+        closesocket(clientSocket);
+        WSACleanup();
+    }
+
+public:
+
+    ConnectionManager() {
+        if (!winsockInit()) {
+            throw runtime_error("Winsock initialization failed");
+        }
+        else {
+            clientConfig();
+        }
+    }
+
+    ~ConnectionManager() {
+        cleanup();
+    }
+
+    SOCKET getClientSocket() const {
+        return this->clientSocket;
+    }
+
+    bool isReady() const {
+        return clientSocket != INVALID_SOCKET;
+    }
+
+    void connectToServer() {
+        if (connect(clientSocket, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) == SOCKET_ERROR) {
+            cerr << "Connect failed with error: " << WSAGetLastError() << endl;
+            return;
+        }
+        cout << "Client connected to server" << endl << endl;
+    }
+};
+
+class CommandHandler {
+
+    SOCKET clientSocket;
+    string baseDirectory = "C:/Users/sofma/client-dir/";
+    string clientDirectory;
+
+public:
+
+    CommandHandler(SOCKET socket) : clientSocket(socket) {}
+
+    ~CommandHandler() {
+        closesocket(clientSocket);
+        WSACleanup();
+    }
+
+    bool isReady() const {
+        return clientSocket != INVALID_SOCKET;
+    }
+};
+
+class Client {
+    ConnectionManager connManager;
+    CommandHandler cmdHandler;
+    SOCKET clientSocket;
+
+public:
+    Client() : connManager(),
+        cmdHandler(move(connManager.getClientSocket())) {
+        clientSocket = connManager.getClientSocket();
+        if (connManager.isReady()) {
+            connManager.connectToServer();
+        }
+        else {
+            throw runtime_error("Client is not ready for connections");
+        }
+    }
+
+    void receiveMessages() {
+        char buffer[4096];
+        while (true) {
+            int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+            if (bytesReceived <= 0) {
+                cerr << "Server disconnected.\n";
+                break;
+            }
+            buffer[bytesReceived] = '\0';
+            cout << buffer << endl;
+        }
+    }
+
+    void processInput() {
+        if (cmdHandler.isReady()) {
+            thread receiveThread([this]() { this->receiveMessages(); });
+            string message;
+            while (true) {
+                getline(std::cin, message);
+                send(clientSocket, message.c_str(), message.size() + 1, 0);
+            }
+        }
+    }
+};
+
 
 int main() {
-	WSADATA wsaData;
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		std::cerr << "WSAStartup failed.\n";
-		return 1;
-	}
-	SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-	if (clientSocket == INVALID_SOCKET) {
-		std::cerr << "Socket creation failed.\n";
-		WSACleanup();
-		return 1;
-	}
-	sockaddr_in serverAddr;
-	serverAddr.sin_family = AF_INET;
-	InetPton(AF_INET, L"127.0.0.1", &serverAddr.sin_addr);
-	serverAddr.sin_port = htons(8080);
-	if (connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) ==
-		SOCKET_ERROR) {
-		std::cerr << "Connection failed.\n";
-		closesocket(clientSocket);
-		WSACleanup();
-		return 1;
-	}
-	std::cout << "Connected to server.\n";
-	// Start a thread to receive messages from the server
-	std::thread receiveThread(receiveMessages, clientSocket);
-	// Main thread to send messages to the server
-	std::string message;
-	while (true) {
-		std::getline(std::cin, message);
-		send(clientSocket, message.c_str(), message.size() + 1, 0);
-	}
-	// Close the socket and clean up
-	closesocket(clientSocket);
-	WSACleanup();
-	return 0;
+    try {
+        Client client;
+        while (true) {
+            client.processInput();
+        }
+    }
+    catch (const runtime_error& e) {
+        cerr << "Error: " << e.what() << endl;
+        return 1;
+    }
+    return 0;
 }
--- server/server/server.cpp
+++ server/server/server.cpp
@@ -11,10 +11,10 @@ using namespace std;
 mutex consoleMutex;
 vector<SOCKET> clients;
 
-void broadcastMessage(const std::string& message, SOCKET senderSocket) {
-	std::lock_guard<std::mutex> lock(consoleMutex);
+void broadcastMessage(const string& message, SOCKET senderSocket) {
+	lock_guard<mutex> lock(consoleMutex);
 	string fullMessage = "Client " + to_string(senderSocket) + ": " + message;
-	std::cout << fullMessage << std::endl;
+	cout << fullMessage << endl;
 	for (SOCKET client : clients) {
 		if (client != senderSocket) {
 			send(client, fullMessage.c_str(), fullMessage.size() + 1, 0);
@@ -28,12 +28,12 @@ void handleClient(SOCKET clientSocket) {
 	while (true) {
 		int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
 		if (bytesReceived <= 0) {
-			std::lock_guard<std::mutex> lock(consoleMutex);
-			std::cout << "Client " << clientSocket << " disconnected.\n";
+			lock_guard<mutex> lock(consoleMutex);
+			cout << "Client " << clientSocket << " disconnected.\n";
 			break;
 		}
 		buffer[bytesReceived] = '\0';
-		std::string message(buffer);
+		string message(buffer);
 		broadcastMessage(message, clientSocket);
 	}
 	closesocket(clientSocket);
@@ -42,44 +42,44 @@ void handleClient(SOCKET clientSocket) {
 int main() {
 	WSADATA wsaData;
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		std::cerr << "WSAStartup failed.\n";
+		cerr << "WSAStartup failed.\n";
 		return 1;
 	}
 	SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 	if (serverSocket == INVALID_SOCKET) {
-		std::cerr << "Socket creation failed.\n";
+		cerr << "Socket creation failed.\n";
 		WSACleanup();
 		return 1;
 	}
 	sockaddr_in serverAddr;
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_addr.s_addr = INADDR_ANY;
-	serverAddr.sin_port = htons(8080);
+	serverAddr.sin_port = htons(12345);
 	if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) ==
 		SOCKET_ERROR) {
-		std::cerr << "Bind failed.\n";
+		cerr << "Bind failed.\n";
 		closesocket(serverSocket);
 		WSACleanup();
 		return 1;
 	}
 	if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
-		std::cerr << "Listen failed.\n";
+		cerr << "Listen failed.\n";
 		closesocket(serverSocket);
 		WSACleanup();
 		return 1;
 	}
-	std::cout << "Server is listening on port 8080...\n";
+	cout << "Server is listening on port 12345...\n";
 	while (true) {
 		SOCKET clientSocket = accept(serverSocket, nullptr, nullptr);
 		if (clientSocket == INVALID_SOCKET) {
-			std::cerr << "Accept failed.\n";
+			cerr << "Accept failed.\n";
 			closesocket(serverSocket);
 			WSACleanup();
 			return 1;
 		}
-		std::lock_guard<std::mutex> lock(consoleMutex);
-		std::cout << "Client " << clientSocket << " connected.\n";
-		std::thread clientThread(handleClient, clientSocket);
+		lock_guard<mutex> lock(consoleMutex);
+		cout << "Client " << clientSocket << " connected.\n";
+		thread clientThread(handleClient, clientSocket);
 		clientThread.detach(); // Detach the thread to allow handling multiple clients concurrently
 	}
 	closesocket(serverSocket);
